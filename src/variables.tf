5e81094db288943a03cd3dbcb20a2b37e8ff53da
Gurdip Sira <87333788+GurdipS5@users.noreply.github.com>
2025-12-18 18:51:00 +0000

    Update variables.tf

--- src/variables.tf
+++ src/variables.tf
@@ -60,3 +60,30 @@ variable "alert_channels" {
   })
   default = {}
 }
+
+variable "url_monitors" {
+  description = "Map of URL monitors to create"
+  type = map(object({
+    name                      = string
+    url                       = string
+    activated                 = optional(bool, true)
+    frequency                 = optional(number, 5)
+    use_global_alert_settings = optional(bool, true)
+    locations                 = optional(list(string), ["eu-west-1"])
+    method                    = optional(string, "GET")
+    follow_redirects          = optional(bool, true)
+    assertions = optional(list(object({
+      source     = string
+      comparison = string
+      target     = string
+    })), [
+      {
+        source     = "STATUS_CODE"
+        comparison = "EQUALS"
+        target     = "200"
+      }
+    ])
+    tags = optional(list(string), [])
+  }))
+  default = {}
+}
