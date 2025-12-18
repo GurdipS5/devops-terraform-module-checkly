6af3013e497949be34e250dfdff1420e37e071f8
Gurdip Sira <87333788+GurdipS5@users.noreply.github.com>
2025-12-18 18:51:26 +0000

    Update url_monitor.tf

--- src/url_monitor.tf
+++ src/url_monitor.tf
@@ -1,20 +1,26 @@
-resource "checkly_url_monitor" "example-url-monitor" {
-  name                      = "Example URL monitor"
-  activated                 = true
-  frequency                 = 2
-  use_global_alert_settings = true
-
-  locations = [
-    "eu-west-1"
-  ]
-
-  request {
-    url = "https://welcome.checklyhq.com"
-
-    assertion {
-      source     = "STATUS_CODE"
-      comparison = "EQUALS"
-      target     = "200"
-    }
-  }
+variable "url_monitors" {
+  description = "Map of URL monitors to create"
+  type = map(object({
+    name                      = string
+    url                       = string
+    activated                 = optional(bool, true)
+    frequency                 = optional(number, 5)
+    use_global_alert_settings = optional(bool, true)
+    locations                 = optional(list(string), ["eu-west-1"])
+    method                    = optional(string, "GET")
+    follow_redirects          = optional(bool, true)
+    assertions = optional(list(object({
+      source     = string
+      comparison = string
+      target     = string
+    })), [
+      {
+        source     = "STATUS_CODE"
+        comparison = "EQUALS"
+        target     = "200"
+      }
+    ])
+    tags = optional(list(string), [])
+  }))
+  default = {}
 }
