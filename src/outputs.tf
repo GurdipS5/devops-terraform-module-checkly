58f867ea86c6e286c808a876859441712a7d0305
Gurdip Sira <87333788+GurdipS5@users.noreply.github.com>
2025-12-18 18:11:10 +0000

    Update outputs.tf

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,3 +1,20 @@
-output "enabled" {
-  value = var.enabled
+# modules/checkly/outputs.tf
+output "check_group_id" {
+  description = "ID of the check group"
+  value       = checkly_check_group.main.id
+}
+
+output "api_check_ids" {
+  description = "Map of API check names to IDs"
+  value       = { for k, v in checkly_check.api : k => v.id }
+}
+
+output "browser_check_ids" {
+  description = "Map of browser check names to IDs"
+  value       = { for k, v in checkly_check.browser : k => v.id }
+}
+
+output "alert_channel_ids" {
+  description = "List of alert channel IDs"
+  value       = local.alert_channel_ids
 }
