0d9abf3154758cdbe0eca68d108d470fb4be843b
Gurdip Sira <87333788+GurdipS5@users.noreply.github.com>
2025-12-18 18:12:12 +0000

    Update variables.tf

--- src/variables.tf
+++ src/variables.tf
@@ -1,5 +1,62 @@
-variable "enabled" {
-  description = "Whether this module is enabled"
-  type        = bool
-  default     = true
+variable "checkly_api_key" {
+  description = "Checkly API key"
+  type        = string
+  sensitive   = true
+}
+
+variable "checkly_account_id" {
+  description = "Checkly account ID"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name (e.g., prod, staging)"
+  type        = string
+  default     = "prod"
+}
+
+variable "default_locations" {
+  description = "Default check locations"
+  type        = list(string)
+  default     = ["eu-west-1", "us-east-1"]
+}
+
+variable "api_checks" {
+  description = "Map of API checks to create"
+  type = map(object({
+    url                    = string
+    method                 = optional(string, "GET")
+    frequency              = optional(number, 5)
+    activated              = optional(bool, true)
+    locations              = optional(list(string), null)
+    expected_status        = optional(string, "200")
+    degraded_response_time = optional(number, 5000)
+    max_response_time      = optional(number, 10000)
+    headers                = optional(map(string), {})
+    tags                   = optional(list(string), [])
+  }))
+  default = {}
+}
+
+variable "browser_checks" {
+  description = "Map of browser checks to create"
+  type = map(object({
+    script_path = string
+    frequency   = optional(number, 10)
+    activated   = optional(bool, true)
+    locations   = optional(list(string), null)
+    tags        = optional(list(string), [])
+  }))
+  default = {}
+}
+
+variable "alert_channels" {
+  description = "Alert channel configurations"
+  type = object({
+    slack_webhook_url = optional(string, null)
+    pagerduty_key     = optional(string, null)
+    opsgenie_api_key  = optional(string, null)
+    email_addresses   = optional(list(string), [])
+  })
+  default = {}
 }
